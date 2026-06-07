8f728db268ed695b346a9f0ff6c1aabe8ed69682
wumingdao <wumingdao070@gmail.com>
2026-06-07 20:35:53 +0800

    feat: implement models endpoint

--- src/routes/openai/videos.ts
+++ src/routes/openai/videos.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
+import { env } from "../../data/env.ts";
 
 const app = new Hono()
+
+const BASE_URL = env.OPENAI_VIDEO_BASE_URL;
+const API_KEY = env.OPENAI_VIDEO_KEY;
 
 app.post('/videos', async (c) => {
     return c.json({
@@ -21,9 +25,17 @@ app.get('/videos/:taskId/content', async (c) => {
 })
 
 app.get('/models', async (c) => {
-    return c.json({
-        message: "/models endpoint is working!"
-    });
+   const data =  await fetch(`${BASE_URL}/models`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        }
+   })
+
+   const json = await data.json();
+
+   return c.json(json);
 })
 
 export default app
